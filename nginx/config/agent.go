6ee9e9f459403206707a0b200b1ccdc593a9eea0
Mike Norgate <mike.norgate@mythical.games>
2023-06-16 13:56:02 +0100

    various deployment fixes

--- nginx/config/agent.go
+++ nginx/config/agent.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -47,6 +47,13 @@ server {
   location / {
     proxy_pass http://registry-backend;
     proxy_next_upstream error timeout http_404 http_500;
+    proxy_buffering off;
+    proxy_request_buffering off;
+    proxy_cache off;
+    proxy_read_timeout 43200s;
+    sendfile on;
+    tcp_nopush on;
+    tcp_nodelay on;
   }
 }
 `
